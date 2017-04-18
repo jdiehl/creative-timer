4e54820efecd88b4da5f2df74d7a0c4a8393a666
Jonathan Diehl <jonathan.diehl@inform-software.com>
2017-04-18 11:03:43 +0200

    let the angle stick to 0/100

--- Timer/TimeSetGestureRecognizer.swift
+++ Timer/TimeSetGestureRecognizer.swift
@@ -18,6 +18,7 @@ func ComputeAngle(point: CGPoint, center: CGPoint) -> CGFloat {
 class TimeSetGestureRecognizer: UIGestureRecognizer {
   
   var angle: CGFloat?
+  var prevAngle: CGFloat?
   
   var onStart: (() -> Void)?
   var onFinish: (() -> Void)?
@@ -34,17 +35,32 @@ class TimeSetGestureRecognizer: UIGestureRecognizer {
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
     if touches.count == 1 || self.view != nil {
+      self.state = .began
       let touch = touches.first!
       let view = self.view!
       let point = touch.location(in: view)
       let center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
-      self.angle = ComputeAngle(point: point, center: center)
-      self.state = .began
+      let newAngle = ComputeAngle(point: point, center: center)
+      
+      // let the angle stick to the 0% / 100% mark
+      if let prevAngle = self.prevAngle {
+        if prevAngle > 0.75 && newAngle < 0.25 {
+          self.angle = 1.0
+        } else if prevAngle < 0.25 && newAngle > 0.75 {
+          self.angle = 0.0
+        } else {
+          self.angle = newAngle
+        }
+      } else {
+        self.angle = newAngle
+      }
+      self.prevAngle = self.angle
     }
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
     self.angle = nil
+    self.prevAngle = nil
     self.state = .ended
     self.onFinish?()
   }

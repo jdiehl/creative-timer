a78ead88469fa68691ca9e796d6a27a868b23df0
Jonathan Diehl <jonathan.diehl@gmail.com>
2021-01-28 19:06:13 +0100

    improved horizontal view

--- Custimer/Views/Timer/TimerView.swift
+++ Custimer/Views/Timer/TimerView.swift
@@ -18,18 +18,18 @@ struct TimerView: View {
       VStack {
         TimerHeaderView(timer: state.timer)
           .padding()
-        
+
         Spacer()
-                                                         
+                                                       
         TimerProgressView(timer: state.timer)
           .frame(maxWidth: 600)
-          .padding()
+          .padding(.horizontal)
         
         Spacer()
         
         TimerProgramView(timer: state.timer)
-          .padding()
-        
+          .padding([.leading, .top, .trailing])
+
         Spacer()
         
         TimerControlsView(timer: state.timer)
@@ -45,5 +45,9 @@ struct TimerView_Previews: PreviewProvider {
   static var previews: some View {
     TimerView()
       .environmentObject(AppState.mock())
+    TimerView()
+      .environmentObject(AppState.mock())
+      .previewLayout(.fixed(width: 2436 / 3.0, height: 1125 / 3.0))
+
   }
 }
